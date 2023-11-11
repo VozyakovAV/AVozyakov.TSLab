c000b051815fcb8142a4d6536dd4d825463783b3
VozyakovAV <andvoz@mail.ru>
2023-11-11 16:34:48 +0400

    .

--- build.bat
+++ build.bat
@@ -1,3 +1,19 @@
+@echo off
+
+rem сборка
 dotnet build
-xcopy /Y "bin\Debug\net7.0\AVozyakov.TSLab.dll" "%LOCALAPPDATA%\TSLab\TSLab 2.0\Handlers"
-xcopy /Y "bin\Debug\net7.0\AVozyakov.TSLab.pdb" "%LOCALAPPDATA%\TSLab\TSLab 2.0\Handlers"
+
+rem копирую в Handlers
+xcopy /y "bin\Debug\net7.0\AVozyakov.TSLab.dll" "%LOCALAPPDATA%\TSLab\TSLab 2.0\Handlers\"
+xcopy /y "bin\Debug\net7.0\AVozyakov.TSLab.pdb" "%LOCALAPPDATA%\TSLab\TSLab 2.0\Handlers\"
+xcopy /h /i /c /k /e /r /y "GoogleSheetsHelper" "%LOCALAPPDATA%\TSLab\TSLab 2.0\Handlers\AVozyakov_Google\"
+
+rem копирую в out
+xcopy /y "bin\Debug\net7.0\AVozyakov.TSLab.dll" "out\build\"
+xcopy /y "bin\Debug\net7.0\AVozyakov.TSLab.pdb" "out\build\"
+xcopy /h /i /c /k /e /r /y "GoogleSheetsHelper" "out\build\AVozyakov_Google\"
+
+rem зипую
+cd "out\build\"
+tar.exe -a -cf ..\AVozyakov.TSLab.zip *
+cd ..\..
